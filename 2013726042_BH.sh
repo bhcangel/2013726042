081be03ed0dbe965d1685174154a09fcc44d67a7
bhcangel <bhcangel@naver.com>
2017-05-07 19:54:45 +0900

    Init

---
+++ 2013726042_BH.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "===print file information ==="
+echo "current directory: " `pwd`
+echo "the number of element : " `ls -l|wc -l`
+
+for var in *
+do
+echo "-----------------------INFORMATION-----------------------"
+echo "1.File type : `stat -c %F $var`"
+echo "2.File size : `stat -c  %s $var`"
+echo "3.last changed : `stat -c %z $var`"
+echo "4.Permission: `stat -c %a $var`"
+done
+
+
+
